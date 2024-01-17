6116f42ab5b39f63920596baf2b41afc583e02ec
Sergey <sergeykaynoy@gmail.com>
2024-01-17 16:39:12 +0700

    close browsers function

--- src/main.py
+++ src/main.py
@@ -1,15 +1,35 @@
+import time
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+url = 'http://local.adspower.net:50325/'
+
 
 def open_browser(browser_id='jd6u9gy'):
-    url = 'http://local.adspower.net:50325/'
-
     endpoint = 'api/v1/browser/start'
 
     api_key = 'c2e4b7646046674561f4f975757cccc0'
+
+    params = {
+        'user_id': browser_id,
+    }
+
+    response = requests.get(
+        url + endpoint,
+        params=params
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('error')
+        return False
+
+
+def close_browser(browser_id='jd6u9gy'):
+    endpoint = 'api/v1/browser/stop'
 
     params = {
         'user_id': browser_id,
@@ -30,7 +50,7 @@ def create_request(driver, url):
     driver.get(url)
 
 
-def main(browser_id=1):
+def main(browser_id: str = None):
     data = open_browser(browser_id)
 
     print(data)
@@ -45,11 +65,12 @@ def main(browser_id=1):
 
     driver = webdriver.Chrome(service=service, options=options)
 
-    for url in df['id'].iterrwos():
-        create_request(driver, url)
+    driver.get('https://www.cian.ru/snyat-kvartiru-1-komn-ili-2-komn/')
+
+    time.sleep(10)
+
+    close_browser(browser_id)
 
 
 if __name__ == '__main__':
-    df = ['rfweq', 'fqwf']
-
-    df['id'].apply(main)
+    main('jd6u9gy')
